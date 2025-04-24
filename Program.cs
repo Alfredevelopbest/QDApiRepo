352e367b093ebb50783f9eb3cae0360f786582f6
Alfredo Gutierrez Olmos <alfred.guol80@gmail.com>
2025-04-24 11:29:43 -0500

    Update Program.cs
    new address added to corrs variable for connection with render.

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: FrontConnection,
         policy =>
         {
-            policy.WithOrigins("http://localhost:5500")
+            policy.WithOrigins("https://alfredevelopbest.github.io/QDWebFront/")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowAnyOrigin();
